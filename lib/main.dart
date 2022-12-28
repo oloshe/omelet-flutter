a4d3911b75ee3326b2ad5e6371eb630f53f4489a
oloshe <jacob.j@qq.com>
2022-12-28 18:32:40 +0800

    feat:update

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,28 @@
 import 'dart:ffi';
+import 'dart:io';
 import 'dart:typed_data';
 
 import 'dart:ui' as ui;
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:omelet/common/media_query.dart';
 import 'package:omelet/ext/index.dart';
 import 'package:omelet/painter/image_editor.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:provider/provider.dart';
+import 'package:tuple/tuple.dart';
 
 void main() {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
   runApp(const MyApp());
 }
 
@@ -23,17 +37,16 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false,
       home: ScreenAdaptor(
-        child: const MyHomePage(title: '坑爹拼图'),
+        child: MyHomePage(),
       ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -46,48 +59,271 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            GestureDetector(
-              onTap: () async {
-                final picker = ImagePicker();
-                final ret = await picker.pickImage(source: ImageSource.gallery);
-                if (ret != null) {
-                  final list = await ret.readAsBytes();
-                  final img = await ImageEditorPainter.loadImage(list);
-                  controller.appendImage(img);
-                }
-              },
-              child: const ColoredBox(
-                color: Colors.grey,
-                child: SizedBox(
-                  width: 200,
-                  height: 100,
-                  child: Icon(Icons.add, color: Colors.white),
-                ),
-              ),
+        title: Text(
+          "Omelet - Image Joint Editor",
+          style: GoogleFonts.maShanZheng(
+            textStyle: const TextStyle(
+              fontSize: 24,
             ),
-            ElevatedButton(
-              onPressed: () {
-                controller.clear();
-              },
-              child: const Text("Clear"),
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: CustomPaint(
-                painter: ImageEditorPainter(
-                  controller: controller,
-                ),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
+      body: MultiProvider(
+          providers: [
+            ChangeNotifierProvider.value(value: controller),
+          ],
+          builder: (context, child) {
+            return SingleChildScrollView(
+              child: SizedBox(
+                width: double.infinity,
+                child: Column(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 10,
+                        left: 10,
+                        right: 10,
+                      ),
+                      child: Row(
+                        children: [
+                          ElevatedButton(
+                            onPressed: () async {
+                              final picker = ImagePicker();
+                              final ret = await picker.pickMultiImage();
+                              for (var file in ret) {
+                                final list = await file.readAsBytes();
+                                final img =
+                                    await ImageEditorPainter.loadImage(list);
+                                controller.appendImage(img);
+                              }
+                            },
+                            child: Row(
+                              children: const [
+                                Icon(Icons.add),
+                                Text("Append"),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                          ElevatedButton(
+                            onPressed: () async {
+                              var status = await Permission.storage.status;
+                              if (!status.isGranted) {
+                                await Permission.storage.request();
+                              }
+                              final img = await controller.export();
+                              await ImageGallerySaver.saveImage(img);
+                            },
+                            style: const ButtonStyle(
+                              backgroundColor:
+                                  MaterialStatePropertyAll(Colors.green),
+                            ),
+                            child: Row(
+                              children: const [
+                                Icon(Icons.save),
+                                SizedBox(width: 2),
+                                Text("Save"),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                          ElevatedButton(
+                            onPressed: () {
+                              controller.clear();
+                            },
+                            style: const ButtonStyle(
+                              backgroundColor:
+                                  MaterialStatePropertyAll(Colors.redAccent),
+                            ),
+                            child: Row(
+                              children: const [
+                                Icon(Icons.clear),
+                                Text("Clear")
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Selector<ImageEditorController, double>(
+                      selector: (context, val) => val.spacing,
+                      builder: (context, val, child) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Row(
+                            children: [
+                              const Text("Spacing"),
+                              const SizedBox(width: 20),
+                              Text(val.toStringAsFixed(1)),
+                              Expanded(
+                                child: Slider(
+                                  value: val,
+                                  min: 0,
+                                  max: 50,
+                                  onChanged: (val) {
+                                    controller.setSpacing(val);
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                    Selector<ImageEditorController, EdgeInsets>(
+                      selector: (context, val) => val.padding,
+                      builder: (context, val, child) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  const Text("Padding Y"),
+                                  const SizedBox(width: 20),
+                                  Text(val.top.toStringAsFixed(1)),
+                                  Expanded(
+                                    child: Slider(
+                                      value: val.top,
+                                      min: 0,
+                                      max: 30,
+                                      onChanged: (newVal) {
+                                        controller.setPadding(
+                                          val.copyWith(
+                                              top: newVal, bottom: newVal),
+                                        );
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  const Text("Padding X"),
+                                  const SizedBox(width: 20),
+                                  Text(val.left.toStringAsFixed(1)),
+                                  Expanded(
+                                    child: Slider(
+                                      value: val.left,
+                                      min: 0,
+                                      max: 30,
+                                      onChanged: (newVal) {
+                                        controller.setPadding(
+                                          val.copyWith(
+                                              left: newVal, right: newVal),
+                                        );
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              const Text("Background"),
+                              const SizedBox(width: 20),
+                              Selector<ImageEditorController, Color>(
+                                selector: (_, val) => val.bgColor,
+                                builder: (_, color, child) => ColoredBox(
+                                  color: color,
+                                  child: SizedBox.fromSize(
+                                    size: const ui.Size.fromRadius(15),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(width: 20),
+                              TextButton(
+                                onPressed: () {
+                                  Color selectColor = controller.bgColor;
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        title: const Text('Pick a color!'),
+                                        content: SingleChildScrollView(
+                                          child: SlidePicker(
+                                            pickerColor: controller.bgColor,
+                                            onColorChanged: (clr) {
+                                              selectColor = clr;
+                                            },
+                                          ),
+                                        ),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () {
+                                              controller
+                                                  .setBgColor(selectColor);
+                                              Navigator.of(context).pop();
+                                            },
+                                            child: const Text('Got it'),
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                },
+                                child: const Text(
+                                  'Pick Color',
+                                  style: TextStyle(
+                                    decoration: TextDecoration.underline,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: SizedBox(
+                        height: 40,
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Preview",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Selector<ImageEditorController, ui.Size>(
+                      selector: (context, val) {
+                        return ui.Size(val.getWidth(), val.getHeight());
+                      },
+                      builder: (context, value, child) {
+                        var scale = value.width == 0
+                            ? 1.0
+                            : ScreenAdaptor.screenWidth / value.width;
+                        return Transform.scale(
+                          scale: scale,
+                          origin: Offset.zero,
+                          alignment: Alignment.topLeft,
+                          child: CustomPaint(
+                            size: value * scale,
+                            painter: ImageEditorPainter(
+                              controller: controller,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }),
     );
   }
 }
--- lib/painter/image_editor.dart
+++ lib/painter/image_editor.dart
@@ -1,15 +1,14 @@
+import 'dart:math';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:omelet/common/media_query.dart';
 
 class ImageEditorPainter extends CustomPainter {
   ImageEditorController controller;
 
-  ImageEditorPainter({required this.controller}): super(repaint: controller);
+  ImageEditorPainter({required this.controller}) : super(repaint: controller);
 
   static Future<ui.Image> loadImage(Uint8List imageData) async {
     final codec = await ui.instantiateImageCodec(imageData);
@@ -19,57 +18,91 @@ class ImageEditorPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    double currTop = controller.padding.top;
-    double currLeft = controller.padding.left;
-    for (var img in controller.images) {
-      final w = img.width.toDouble();
-      final h = img.height.toDouble();
-      final Rect src = Rect.fromLTWH(0, 0, w, h);
-      late final Rect dest;
-      if (controller.isHorizontal) {
-        final dh = ScreenAdaptor.screenHeight;
-        final scale = h / dh;
-        final dw = w / scale;
-        dest = Rect.fromLTWH(currLeft, currTop, dw, dh);
-        currLeft += dw + controller.spacing;
-      } else {
-        final dw = ScreenAdaptor.screenWidth;
-        final scale = w / dw;
-        final dh = h / scale;
-        dest = Rect.fromLTWH(currLeft, currTop, dw, dh);
-        currTop += dh + controller.spacing;
-      }
-      canvas.drawImageRect(img, src, dest, Paint());
-    }
+    controller.paint(canvas, size);
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return controller.images.length != (oldDelegate as ImageEditorPainter).controller.images.length;
+    return controller.images.length !=
+        (oldDelegate as ImageEditorPainter).controller.images.length;
   }
 }
 
 class ImageEditorController with ChangeNotifier {
   List<ui.Image> images = [];
+
   /// 是否是水平
   bool isHorizontal = false;
+
   /// 间隔
   double spacing = 0;
+
   /// 白边
   EdgeInsets padding = EdgeInsets.zero;
+
   /// 背景颜色
   Color bgColor = Colors.white;
 
+  double _maxImgWidth = 0;
+  double _maxImgHeight = 0;
+
+  /// 宽度
+  double getWidth() {
+    if (images.isEmpty) {
+      return 0;
+    }
+    if (isHorizontal) {
+      final dh = getHeight();
+      final totalWidth = images.map((e) {
+        final scale = e.height / dh;
+        return e.width / scale;
+      }).reduce((a, b) => a + b);
+      return totalWidth + totalSpacing + padding.left + padding.right;
+    } else {
+      return _maxImgWidth + padding.left + padding.right;
+    }
+  }
+
+  /// 高度
+  double getHeight() {
+    if (images.isEmpty) {
+      return 0;
+    }
+    if (isHorizontal) {
+      return _maxImgHeight;
+    } else {
+      final dw = getWidth() - padding.left - padding.right;
+      final totalHeight = images.map((e) {
+        final scale = e.width / dw;
+        return e.height / scale;
+      }).reduce((a, b) => a + b);
+      return totalHeight + totalSpacing + padding.top + padding.bottom;
+    }
+  }
+
+  /// 总共的间隙
+  double get totalSpacing => spacing * (images.length - 1);
+
+  _updateMaxImgSize() {
+    _maxImgWidth = images.map((e) => e.width).reduce(max).toDouble();
+    _maxImgHeight = images.map((e) => e.height).reduce(max).toDouble();
+  }
+
+  /// 添加一张图片
   appendImage(ui.Image img) {
     images.add(img);
+    _updateMaxImgSize();
     notifyListeners();
   }
 
+  /// 清除所有
   clear() {
     images.clear();
+    _updateMaxImgSize();
     notifyListeners();
   }
 
+  /// 设置是否水平分布
   setHorizontal(bool val) {
     if (isHorizontal != val) {
       isHorizontal = val;
@@ -77,6 +110,7 @@ class ImageEditorController with ChangeNotifier {
     }
   }
 
+  /// 设置间隙
   setSpacing(double val) {
     if (spacing != val) {
       spacing = val;
@@ -84,6 +118,7 @@ class ImageEditorController with ChangeNotifier {
     }
   }
 
+  /// 设置 padding
   setPadding(EdgeInsets val) {
     if (padding != val) {
       padding = val;
@@ -91,10 +126,52 @@ class ImageEditorController with ChangeNotifier {
     }
   }
 
+  /// 设置背景颜色
   setBgColor(Color c) {
     if (bgColor != c) {
       bgColor = c;
       notifyListeners();
     }
+  }
+
+  void paint(Canvas canvas, Size size) {
+    double maxWidth = images.map((e) => e.width).reduce(max).toDouble();
+    double maxHeight = images.map((e) => e.height).reduce(max).toDouble();
+    double currTop = padding.top;
+    double currLeft = padding.left;
+    final bgPaint = Paint()..color = bgColor;
+    final bgRect = Rect.fromLTWH(0, 0, getWidth(), getHeight());
+    canvas.drawRect(bgRect, bgPaint);
+    for (var img in images) {
+      final w = img.width.toDouble();
+      final h = img.height.toDouble();
+      final Rect src = Rect.fromLTWH(0, 0, w, h);
+      late final Rect dest;
+      if (isHorizontal) {
+        final scale = h / maxHeight;
+        final dw = w / scale;
+        dest = Rect.fromLTWH(currLeft, currTop, dw, maxHeight);
+        currLeft += dw + spacing;
+      } else {
+        final scale = w / maxWidth;
+        final dh = h / scale;
+        dest = Rect.fromLTWH(currLeft, currTop, maxWidth, dh);
+        currTop += dh + spacing;
+      }
+      canvas.drawImageRect(img, src, dest, Paint());
+    }
+  }
+
+  Future<Uint8List> export() async {
+    final recorder = ui.PictureRecorder();
+    final canvas = Canvas(recorder);
+    final w = getWidth();
+    final h = getHeight();
+    paint(canvas, ui.Size(w, h));
+    final pic = recorder.endRecording();
+    print(">>> $w $h");
+    final img = await pic.toImage(w.toInt(), h.toInt());
+    final pngBytes = await img.toByteData(format: ui.ImageByteFormat.png);
+    return Uint8List.view(pngBytes!.buffer);
   }
 }
